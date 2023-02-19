bdd3fd54e8e5b9904d5ddf8df245f11f8055f028
ohsumin1 <otnals25@naver.com>
2023-02-19 16:46:02 +0900

    fix: api 조회 버그 수정 - query params 변경시 page값 0으로 설정 - page, param 변경시 한번만 api 조회하도록 수정 - api 조회가 필요없는 필터라도 api 조회하고 필터링 하도록 수정(스크롤시 필터링 안되는 이슈) - 처음 렌더링시 api 조회 한번만 하도록 수정 - 처음 렌더링시 IO 감지 안되도록 수정

--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -4,74 +4,64 @@ import { getDataLength } from "../utils/util"
 
 const CharacterList = ({ useFilterContext, clickedRefresh }) => {
   const [characterList, setCharacterList] = useState([]);
-  const [oldFilterStr, setOldFilterStr] = useState([]);
+  const [params, setParams] = useState('');
   const [page, setPage] = useState(0);
+  const [isInitialLoad, setIsInitialLoad] = useState(true);
   const [selectedFilters] = useFilterContext();
   const loader = useRef(null);
 
   useEffect(() => {
-    const options = {
-      root: null,
-      rootMargin: "100px",
-      threshold: 1.0
-    };
+    fetchData(params);
+  }, [page, params]);
 
-    const observer = new IntersectionObserver(handleObserver, options);
-    if (loader.current) {
-      observer.observe(loader.current)
+  useEffect(() => {
+    if (!isInitialLoad) {
+      changeParams();
+      setPage(0);
     }
-  }, []);
-
-  useEffect(() => {
-    fetchData();
-  }, [page]);
-
-  useEffect(() => {
-    searchCharacter();
   }, [selectedFilters])
 
-  const handleObserver = (entries) => {
-    if (entries[0].isIntersecting) {
-      setPage(prevPage => prevPage + 1);
-    }
-  }
-
-  const searchCharacter = () => {
-    const queryFilters = Object.fromEntries(Object.entries(selectedFilters)
-      .filter(([, value]) => value !== ''));
-    const newFilterStr = Object.keys(queryFilters).join(",");
-
-    /**
-     * API 조회가 필요없는 필터를 선택한 경우 API 조회를 하지 않고 데이터를 필터링 해준다.
-     * 해당 필터를 해제한 경우 API를 조회한다.
-     */
-    if (newFilterStr === oldFilterStr) {
-      if (filterList()) {
-        return;
+  useEffect(() => { 
+    const observer = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting) {
+        setPage(prevPage => prevPage + 1);
       }
-    } 
-    const params = new URLSearchParams(queryFilters);
-    fetchData(params); 
-    setOldFilterStr(newFilterStr);
+    }, );
+
+    if (loader.current) {
+      observer.observe(loader.current);
+    }
+
+    return () => {
+      if (loader.current) {
+        observer.unobserve(loader.current);
+      }
+    }
+  }, [loader.current]);
+
+  const changeParams = () => {
+    const queryFilters = Object.fromEntries(Object.entries(selectedFilters)
+      .filter(([, value]) => value !== '')); 
+    const newParams = new URLSearchParams(queryFilters);
+    setParams(newParams);
   }
 
   const fetchData = (params) => {
     const PAGE_SIZE = 10;
     fetch(`https://www.anapioficeandfire.com/api/characters?page=${page}&pageSize=${PAGE_SIZE}&${params}`)
       .then(response => response.json())
-      .then(result => setCharacterList(prevState => [...prevState, ...result]))
+      .then(result => page > 0 ? [...characterList, ...result] : result)
+      .then(result => filterList(result))
+      .then(result => setCharacterList(result))
+      .then(isInitialLoad && setIsInitialLoad(false))
       .catch(error => console.log('error', error));
   }
 
-  const filterList = () => {
-    /**
-     * 필터링을 할 경우 필터링 후 true를 반환, 필터링을 하지 않을 경우 false를 반환해서 API를 조회하도록 한다.
-     */
+  const filterList = (list) => {
     if (selectedFilters.hasOwnProperty('tvSeries')) {
-      setCharacterList(characterList.filter((el) => getDataLength(el.tvSeries) === 0));
-      return true;
-    } 
-    return false;
+      return list.filter((el) => getDataLength(el.tvSeries) === 0);
+    }
+    return list;
   }
 
   return (
@@ -83,7 +73,7 @@ const CharacterList = ({ useFilterContext, clickedRefresh }) => {
           clickedRefresh={clickedRefresh}
         />
       ))}
-      <div ref={loader} />
+      {characterList.length > 0 && <div ref={loader} />}
     </div>
   )
 }
